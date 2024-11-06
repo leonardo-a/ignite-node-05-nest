11a597c579ec7fcbe0bc40f5627d2f5880944385
Leonardo <leonardoa.dev@gmail.com>
2024-11-05 23:28:37 -0300

    Controller de criação de pergunta

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user.decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -11,9 +22,34 @@ export class CreateQuestionController {
 
   @Post()
   @HttpCode(201)
-  async handle(@CurrentUser() user: UserPayload) {
-    return {
-      user,
-    }
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+
+    const userId = user.sub
+
+    const slug = this.convertToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  convertToSlug(value: string): string {
+    return value
+      .toLowerCase() // Converte para minúsculas
+      .normalize('NFD') // Decompõe os caracteres em suas partes (e.g., "á" vira "a" + acento)
+      .replace(/[\u0300-\u036f]/g, '') // Remove os acentos e cedilhas
+      .replace(/[^a-z0-9\s-]/g, '') // Remove caracteres especiais
+      .replace(/\s+/g, '-') // Substitui espaços por hífen
+      .replace(/-+/g, '-') // Remove hífens duplicados
+      .replace(/^-|-$/g, '') // Remove hífen do início e do fim
   }
 }
