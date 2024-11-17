ed19f9be532a23ff645847035710839bc132a9eb
Leonardo <leonardoa.dev@gmail.com>
2024-11-17 19:38:43 -0300

    Criando Mappers do Prisma

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -0,0 +1,20 @@
+import { Attachment as PrismaAttachment } from '@prisma/client'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+
+export class PrismaAnswerAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return AnswerAttachment.create(
+      {
+        answerId: new UniqueEntityID(raw.answerId),
+        attachmentId: new UniqueEntityID(raw.id),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
---
+++ src/infra/database/prisma/mappers/prisma-question-attachment-mapper.ts
@@ -0,0 +1,20 @@
+import { Attachment as PrismaAttachment } from '@prisma/client'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
+
+export class PrismaQuestionAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): QuestionAttachment {
+    if (!raw.questionId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return QuestionAttachment.create(
+      {
+        questionId: new UniqueEntityID(raw.questionId),
+        attachmentId: new UniqueEntityID(raw.id),
+      },
+      new UniqueEntityID(raw.id),
+    )
+  }
+}
