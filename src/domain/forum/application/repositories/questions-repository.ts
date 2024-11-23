64957d404b9210ecfe5368d38c66eb1714bac5f8
Leonardo <leonardoa.dev@gmail.com>
2024-11-23 17:24:21 -0300

    Value Object detalhes da pergunta

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,10 +1,13 @@
 import type { PaginationParams } from '@/core/repositories/pagination-params'
 import type { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionDetails } from '@/domain/forum/enterprise/entities/value-objects/question-details'
 
 export abstract class QuestionsRepository {
   abstract findById(id: string): Promise<Question | null>
 
   abstract findBySlug(slug: string): Promise<Question | null>
+
+  abstract findDetailsBySlug(slug: string): Promise<QuestionDetails | null>
 
   abstract findManyRecent(params: PaginationParams): Promise<Question[]>
 
