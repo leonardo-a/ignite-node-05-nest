6c56066f975909d2ecd1a3b496b5330a07325f20
Leonardo <leonardoa.dev@gmail.com>
2024-11-24 16:19:31 -0300

    Criando Service do Redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common'
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
 
-@Module({})
+@Module({
+  imports: [EnvModule],
+  providers: [EnvService],
+})
 export class CacheModule {}
